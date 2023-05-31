c21955839173dcc17013754e27b0a040151178f5
AveryKing <kavery086@gmail.com>
2023-05-30 21:46:13 -0400

    route mapped to list

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -44,6 +44,6 @@ List<Game> games = new()
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/games", () => games);
 
 app.Run();
