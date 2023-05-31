cd77039fe4058c8c910ae8cf5be1e6ca1569b7f0
AveryKing <kavery086@gmail.com>
2023-05-31 01:42:11 -0400

    range added to price

--- GameStore.Api/Entities/Game.cs
+++ GameStore.Api/Entities/Game.cs
@@ -8,8 +8,8 @@ public class Game
 
     [Required] [StringLength(50)] public required string Name { get; set; }
 
-    public required string Genre { get; set; }
-    public decimal Price { get; set; }
+    [Required] [StringLength(20)] public required string Genre { get; set; }
+    [Range(1, 100)] public decimal Price { get; set; }
     public DateTime ReleaseDate { get; set; }
     public required string ImageUri { get; set; }
 }
