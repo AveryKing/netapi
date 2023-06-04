b50dfed1d0592c81a60695f4f29b8e11fd7946f0
AveryKing <kavery086@gmail.com>
2023-06-04 11:59:19 -0400

    Dtos added

--- GameStore.Api/Entities/Game.cs
+++ GameStore.Api/Entities/Game.cs
@@ -9,5 +9,5 @@ public class Game
     [Required] [StringLength(20)] public required string Genre { get; set; }
     [Range(1, 100)] public decimal Price { get; set; }
     public DateTime ReleaseDate { get; set; }
-    public required string ImageUri { get; set; }
+    [Url] [StringLength(100)] public required string ImageUri { get; set; }
 }
