348cf34ad1e1762e48c11e909b285a0f5052327b
AveryKing <kavery086@gmail.com>
2023-06-07 20:15:11 -0400

    DbContext registered with Services

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,11 +1,14 @@
+using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 using GameStore.Api.Repositories;
+using MySql.EntityFrameworkCore.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<IGamesRepository, InMemGamesRepository>();
 
 var connString = builder.Configuration.GetConnectionString("GameStoreContext");
+builder.Services.AddMySQLServer<GameStoreContext>(connString!);
 
 var app = builder.Build();
 
